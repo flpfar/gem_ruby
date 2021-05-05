c5f9ac5ccf1ae37bc36f08c30faef27728b55939
Felipe Augusto Rosa <flp.far@gmail.com>
2021-05-05 06:14:23 -0300

    refactor: bubble sort

--- lib/gem_ruby.rb
+++ lib/gem_ruby.rb
@@ -1,9 +1,12 @@
 # frozen_string_literal: true
 
 require_relative "gem_ruby/version"
+
 require_relative "gem_ruby/algorithms/searching/linear_search.rb"
 require_relative "gem_ruby/algorithms/searching/binary_search.rb"
 require_relative "gem_ruby/algorithms/searching/jump_search.rb"
+
+require_relative "gem_ruby/algorithms/sorting/bubble_sort.rb"
 
 module GemRuby
   class Error < StandardError; end
