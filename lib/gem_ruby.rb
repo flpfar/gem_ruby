7a00d559e6056533e37e8821459b3d5b02a40092
Felipe Augusto Rosa <flp.far@gmail.com>
2021-05-04 21:41:15 -0300

    refactor: linear search

--- lib/gem_ruby.rb
+++ lib/gem_ruby.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 require_relative "gem_ruby/version"
+require_relative "gem_ruby/algorithms/searching/linear_search.rb"
 
 module GemRuby
   class Error < StandardError; end
