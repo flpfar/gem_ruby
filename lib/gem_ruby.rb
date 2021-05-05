710a4fd5de2273a7377561b171fd66ada1188ed3
Felipe Augusto Rosa <flp.far@gmail.com>
2021-05-05 08:03:46 -0300

    refactor: minimum sum product

--- lib/gem_ruby.rb
+++ lib/gem_ruby.rb
@@ -12,6 +12,7 @@ require_relative "gem_ruby/algorithms/sorting/merge_sort.rb"
 
 require_relative "gem_ruby/algorithms/greedy/maximize_array.rb"
 require_relative "gem_ruby/algorithms/greedy/minimum_product.rb"
+require_relative "gem_ruby/algorithms/greedy/minimum_sum_product.rb"
 
 require_relative "gem_ruby/challenges/string/missing_characters.rb"
 require_relative "gem_ruby/challenges/string/count.rb"
