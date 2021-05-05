94ffaeec9de597ff1ce773b1dcebfda29bc634c4
Felipe Augusto Rosa <flp.far@gmail.com>
2021-05-05 20:44:14 -0300

    refactor: missing characters

--- lib/gem_ruby.rb
+++ lib/gem_ruby.rb
@@ -10,6 +10,8 @@ require_relative "gem_ruby/algorithms/sorting/bubble_sort.rb"
 require_relative "gem_ruby/algorithms/sorting/counting_sort.rb"
 require_relative "gem_ruby/algorithms/sorting/merge_sort.rb"
 
+require_relative "gem_ruby/challenges/string/missing_characters.rb"
+
 module GemRuby
   class Error < StandardError; end
   # Your code goes here...
