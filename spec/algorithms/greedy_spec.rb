eed415903d3a42cc4a1dc7dd89086d135e249207
Felipe Augusto Rosa <flp.far@gmail.com>
2021-05-05 08:01:43 -0300

    test: minimum product

--- spec/algorithms/greedy_spec.rb
+++ spec/algorithms/greedy_spec.rb
@@ -5,4 +5,10 @@ RSpec.describe Algorithms::Greedy do
     it { expect(Algorithms::Greedy.maximize_array([-2, 0, 5, -1, 2], 4)).to eq 10 }
     it { expect(Algorithms::Greedy.maximize_array([9, 8, 8, 5], 3)).to eq 20 }
   end
+
+  describe ".minimum_product" do
+    it { expect(Algorithms::Greedy.minimum_product([-1, -1, -2, 4, 3])).to eq(-24) }
+    it { expect(Algorithms::Greedy.minimum_product([-1, 0])).to eq(-1) }
+    it { expect(Algorithms::Greedy.minimum_product([0, 0, 0])).to eq 0 }
+  end
 end
