cd118b0618bb6cbd4be89eecfc8e35f980f2ea4d
Felipe Augusto Rosa <flp.far@gmail.com>
2021-05-05 06:36:19 -0300

    test: counting sort

--- spec/algorithms/sorting_spec.rb
+++ spec/algorithms/sorting_spec.rb
@@ -8,4 +8,9 @@ RSpec.describe Algorithms::Sorting do
         .to eq [5, 10, 14, 15, 16, 36, 39, 67, 82, 90]
     }
   end
+
+  describe ".counting_sort" do
+    it { expect(Algorithms::Sorting.counting_sort([6, 3, 1, 2, 4, 8, 1, 2, 4])).to eq [1, 1, 2, 2, 3, 4, 4, 6, 8] }
+    it { expect(Algorithms::Sorting.counting_sort([1, 4, 1, 2, 7, 5, 2, 0])).to eq [0, 1, 1, 2, 2, 4, 5, 7] }
+  end
 end
