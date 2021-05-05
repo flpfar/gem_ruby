b5e4a265612eaf96f8e4e0b46ef981d5c6e72061
Felipe Augusto Rosa <flp.far@gmail.com>
2021-05-04 21:54:19 -0300

    test: jump search

--- spec/algorithms/searching_spec.rb
+++ spec/algorithms/searching_spec.rb
@@ -17,4 +17,13 @@ RSpec.describe Algorithms::Searching do
     it { expect(Algorithms::Searching.binary_search([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 10)).to eq 0 }
     it { expect(Algorithms::Searching.binary_search([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 90)).to eq(-1) }
   end
+
+  describe ".jump_search" do
+    it { expect(Algorithms::Searching.jump_search([1, 2, 3], 3)).to eq 2 }
+    it { expect(Algorithms::Searching.jump_search([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 110)).to eq 7 }
+    it { expect(Algorithms::Searching.jump_search([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 175)).to eq(-1) }
+    it { expect(Algorithms::Searching.jump_search([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 170)).to eq 9 }
+    it { expect(Algorithms::Searching.jump_search([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 10)).to eq 0 }
+    it { expect(Algorithms::Searching.jump_search([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 90)).to eq(-1) }
+  end
 end
