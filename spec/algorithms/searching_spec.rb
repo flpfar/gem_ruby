194cb8ebdfbe2ef5e09dc7281bbb2e388bf82cc7
Felipe Augusto Rosa <flp.far@gmail.com>
2021-05-04 21:50:00 -0300

    test: binary search

--- spec/algorithms/searching_spec.rb
+++ spec/algorithms/searching_spec.rb
@@ -8,4 +8,13 @@ RSpec.describe Algorithms::Searching do
     it { expect(Algorithms::Searching.linear_search([10, 20, 80, 30, 60, 50, 110, 100, 130, 170], 170)).to eq 9 }
     it { expect(Algorithms::Searching.linear_search([10, 20, 80, 30, 60, 50, 110, 100, 130, 170], 10)).to eq 0 }
   end
+
+  describe ".binary_search" do
+    it { expect(Algorithms::Searching.binary_search([1, 2, 3], 3)).to eq 2 }
+    it { expect(Algorithms::Searching.binary_search([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 110)).to eq 7 }
+    it { expect(Algorithms::Searching.binary_search([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 175)).to eq(-1) }
+    it { expect(Algorithms::Searching.binary_search([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 170)).to eq 9 }
+    it { expect(Algorithms::Searching.binary_search([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 10)).to eq 0 }
+    it { expect(Algorithms::Searching.binary_search([10, 20, 30, 50, 60, 80, 100, 110, 130, 170], 90)).to eq(-1) }
+  end
 end
