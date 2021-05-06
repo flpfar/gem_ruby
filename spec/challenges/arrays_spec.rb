e75859cf700c206b7c6cc2feb124937f810c4cb1
Felipe Augusto Rosa <flp.far@gmail.com>
2021-05-06 06:40:24 -0300

    test: reorder index

--- spec/challenges/arrays_spec.rb
+++ spec/challenges/arrays_spec.rb
@@ -10,4 +10,12 @@ RSpec.describe Challenges::Arrays do
     it { expect(Challenges::Arrays.reversal([1, 2, 3, 4, 5, 6, 7, 8, 9], 3)).to eq [7, 8, 9, 1, 2, 3, 4, 5, 6] }
     it { expect(Challenges::Arrays.reversal([121, 232, 33, 43, 5], 2)).to eq [43, 5, 121, 232, 33] }
   end
+
+  describe ".reorder_index" do
+    it { expect(Challenges::Arrays.reorder_index([10, 11, 12], [1, 0, 2])).to eq [[11, 10, 12], [0, 1, 2]] }
+    it {
+      expect(Challenges::Arrays.reorder_index([50, 40, 70, 60, 90], [3, 0, 4, 1, 2])).to eq [[40, 60, 90, 50, 70],
+                                                                                             [0, 1, 2, 3, 4]]
+    }
+  end
 end
